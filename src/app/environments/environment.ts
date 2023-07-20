fb89a329001c1bae6404ba176d95a90704d7956c
Josh Fischer <josh@joshfischer.io>
2023-07-20 07:53:38 -0500

    update props

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   iridium: {
-    domain: 'http://localhost:8381/',
+    domain: 'https://ENTER_YOUR_TENANT_NAME_HERE.iridium.software/',
     redirectUri: 'http://localhost:4300/callback',
     clientId: 'ENTER_YOUR_CLIENT_ID_HERE',
     errorPath: '/error'
